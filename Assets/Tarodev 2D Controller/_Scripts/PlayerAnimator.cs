f01e0575d959bf745846f271c5d13b05254385ff
Shane Gadsby <schme16@gmail.com>
2022-04-02 21:26:14 +1000

    animations added (not good, just present), silly sounds added, character controller present

---
+++ Assets/Tarodev 2D Controller/_Scripts/PlayerAnimator.cs
@@ -0,0 +1,149 @@
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace TarodevController {
+	/// <summary>
+	/// This is a pretty filthy script. I was just arbitrarily adding to it as I went.
+	/// You won't find any programming prowess here.
+	/// This is a supplementary script to help with effects and animation. Basically a juice factory.
+	/// </summary>
+	public class PlayerAnimator : MonoBehaviour {
+		[SerializeField] private Animator _anim;
+		[SerializeField] private AudioSource _source;
+		[SerializeField] private LayerMask _groundMask;
+		[SerializeField] private ParticleSystem _jumpParticles, _launchParticles;
+		[SerializeField] private ParticleSystem _moveParticles, _landParticles;
+		[SerializeField] private AudioClip[] _footsteps;
+		[SerializeField] private float _maxTilt = .1f;
+		[SerializeField] private float _tiltSpeed = 1;
+		[SerializeField, Range(1f, 3f)] private float _maxIdleSpeed = 2;
+		[SerializeField] private float _maxParticleFallSpeed = -40;
+		[SerializeField] private float walkSoundCooldown = 0;
+
+		private IPlayerController _player;
+		private bool _playerGrounded;
+		private ParticleSystem.MinMaxGradient _currentGradient;
+		private Vector2 _movement;
+
+		void Awake() => _player = GetComponentInParent<IPlayerController>();
+
+		void Update() {
+			walkSoundCooldown -= Mathf.Max(Time.deltaTime, 0);
+			if (_player == null) return;
+
+			// Flip the sprite
+			if (_player.Input.X != 0) {
+				transform.localScale = new Vector3(_player.Input.X > 0 ? 1 : -1, 1, 1);
+
+				//Set the player facing direction
+				if (_player.Input.X > 0) {
+					transform.parent.parent.localScale = new Vector3(-1, transform.parent.parent.localScale.y, transform.parent.parent.localScale.z);
+					Debug.Log(1);
+				}
+				else {
+					transform.parent.parent.localScale = new Vector3(1, transform.parent.parent.localScale.y, transform.parent.parent.localScale.z);
+					Debug.Log(2);
+				}
+
+				if (_player.Grounded) {
+					//Start walk anim here
+					_anim.ResetTrigger(GroundedKey);
+					_anim.ResetTrigger(IdleKey);
+					_anim.SetTrigger(WalkKey);
+					if (walkSoundCooldown < 0) {
+						walkSoundCooldown = 0.5f;
+						_source.PlayOneShot(_footsteps[2]);
+					}
+				}
+			}
+			else {
+				if (_player.Grounded) {
+					//Start Idle anim here
+					_anim.ResetTrigger(GroundedKey);
+					_anim.ResetTrigger(WalkKey);
+					_anim.SetTrigger(IdleKey);
+				}
+			}
+
+			// Lean while running
+			var targetRotVector = new Vector3(0, 0,
+				Mathf.Lerp(-_maxTilt, _maxTilt, Mathf.InverseLerp(-1, 1, _player.Input.X)));
+
+			/*_anim.transform.rotation = Quaternion.RotateTowards(_anim.transform.rotation,
+				Quaternion.Euler(targetRotVector), _tiltSpeed * Time.deltaTime);*/
+
+			// Speed up idle while running
+			// _anim.SetFloat(IdleSpeedKey, Mathf.Lerp(1, _maxIdleSpeed, Mathf.Abs(_player.Input.X)));
+
+			// Splat
+			if (_player.LandingThisFrame) {
+				_anim.ResetTrigger(IdleKey);
+				_anim.SetTrigger(GroundedKey);
+				_source.PlayOneShot(_footsteps[0]);
+			}
+
+			// Jump effects
+			if (_player.JumpingThisFrame) {
+				_anim.ResetTrigger(IdleKey);
+				_anim.ResetTrigger(WalkKey);
+				_anim.SetTrigger(JumpKey);
+				_anim.ResetTrigger(GroundedKey);
+				_source.PlayOneShot(_footsteps[1]);
+
+
+				// Only play particles when grounded (avoid coyote)
+				if (_player.Grounded) {
+					SetColor(_jumpParticles);
+					SetColor(_launchParticles);
+					_jumpParticles.Play();
+				}
+			}
+
+			// Play landing effects and begin ground movement effects
+			if (!_playerGrounded && _player.Grounded) {
+				_playerGrounded = true;
+				_moveParticles.Play();
+				_landParticles.transform.localScale =
+					Vector3.one * Mathf.InverseLerp(0, _maxParticleFallSpeed, _movement.y);
+				SetColor(_landParticles);
+				_landParticles.Play();
+			}
+			else if (_playerGrounded && !_player.Grounded) {
+				_playerGrounded = false;
+				_moveParticles.Stop();
+			}
+
+			// Detect ground color
+			var groundHit = Physics2D.Raycast(transform.position, Vector3.down, 2, _groundMask);
+			if (groundHit && groundHit.transform.TryGetComponent(out SpriteRenderer r)) {
+				_currentGradient = new ParticleSystem.MinMaxGradient(r.color * 0.9f, r.color * 1.2f);
+				SetColor(_moveParticles);
+			}
+
+			_movement = _player.RawMovement; // Previous frame movement is more valuable
+		}
+
+		private void OnDisable() {
+			_moveParticles.Stop();
+		}
+
+		private void OnEnable() {
+			_moveParticles.Play();
+		}
+
+		void SetColor(ParticleSystem ps) {
+			var main = ps.main;
+			main.startColor = _currentGradient;
+		}
+
+		#region Animation Keys
+
+		private static readonly int GroundedKey = Animator.StringToHash("Grounded");
+		private static readonly int IdleSpeedKey = Animator.StringToHash("IdleSpeed");
+		private static readonly int JumpKey = Animator.StringToHash("Jump");
+		private static readonly int WalkKey = Animator.StringToHash("Walk");
+		private static readonly int IdleKey = Animator.StringToHash("Idle");
+
+		#endregion
+	}
+}
